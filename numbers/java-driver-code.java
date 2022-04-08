b08600c81670ccda4f3f7a10106b1f62a7dc360e
rakeshsingh1008 <95102266+rakeshsingh1008@users.noreply.github.com>
2022-04-08 13:41:34 +0530

    magic number

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -22,7 +22,65 @@ public class Main {
       	// Your code goes here
         // n is the given input
       
-        String result = "Special";
+      int lenght = (int)(Math.log10(number)+1);
+      
+      int[] arr = new int[length];
+      int i =0;
+      int temp;
+      
+      
+      while(n!= 0)
+      {
+        temp = n %10;
+        n/=10;
+        arr[i++] = temp;
+      }
+      
+      int[] b = new int[length];
+      int j = n;
+      for(int i =0;i<n;i++)
+      {
+        b[j-1] = a[i];
+        j = j -1;
+      }
+      
+      int left = 0; int right = 0;
+      if(n %2 == 0)
+      {
+        for(int d = 0;d<n/2;d++)
+        {
+          left = left*10 + b[d];
+        }
+        
+        for(int c = n/2;c<n;c++)
+        {
+          right = right*10 + b[c];
+        }
+      }
+      else{
+        
+        for(int d = 0;d<n/2;d++)
+        {
+          left = left*10 + b[d];
+        }
+        
+        for(int c = (n/2)+1;c<n;c++)
+        {
+          right = right*10 + b[c];
+        }
+      }
+      
+      if(left > right)
+      {
+         String result = "magic number";
         return result;
+      }
+      else
+      {
+         String result = "normal number";
+        return result;
+      }
+       
+     
     }
 }
